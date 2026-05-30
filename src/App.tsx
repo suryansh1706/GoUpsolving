a7b591e5687433fb6aaab2ee85cdfba89fae3d84
Suryansh Rastogi <srbrothers1340@gmail.com>
2026-05-31 01:06:27 +0530

    installed vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
 import { CodeforcesUpsolveTracker } from "./features/components";
 import "./shared/global.css";
 
 export function App() {
-  return <CodeforcesUpsolveTracker />;
+  return (
+    <>
+      <CodeforcesUpsolveTracker />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
